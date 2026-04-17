81831f48ec80c6d1d1c6cb121707748af062ada2
Nazar <berknazar064@gmail.com>
2026-04-17 22:19:44 +0300

    update

--- SurveyPortal.API/DTOs/SurveyDto.cs
+++ SurveyPortal.API/DTOs/SurveyDto.cs
@@ -7,5 +7,6 @@
         public string? Description { get; set; } //Tanım
         public bool IsActive { get; set; }
         public int CategoryId { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
