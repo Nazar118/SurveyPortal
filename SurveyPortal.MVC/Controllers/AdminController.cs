db62114be8c536d73e552822766d633389a5bbad
Nazar <berknazar064@gmail.com>
2026-04-17 18:15:27 +0300

    update

--- SurveyPortal.MVC/Controllers/AdminController.cs
+++ SurveyPortal.MVC/Controllers/AdminController.cs
@@ -8,5 +8,10 @@ namespace SurveyPortal.MVC.Controllers
         {
             return View();
         }
+
+        public IActionResult SurveyList()
+        {
+            return View();
+        }
     }
 }
